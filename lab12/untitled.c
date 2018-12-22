53c2d4a66f334c3d72ffa25355ab1bc170f38844
stalt4 <mdima4266@gmail.com>
2018-12-22 14:51:24 +0200

    Fix bug

--- lab12/untitled.c
+++ lab12/untitled.c
@@ -4,11 +4,12 @@
 #define R 8
 
 
-unsigned char data[R][R], swap, r, c;
+unsigned char data[R][R], swap, r, c,index = R;
+
 
 
 int main(void) 
-{
+{ 
 	srand(time(NULL));
 
 	printf("\nRandom 2d massive:\n");
@@ -18,15 +19,14 @@ int main(void)
 		for ( c = 0; c < R; c++)
 		{
 			data[r][c] = rand() % 100;
-
 			printf(" %3d ", data[r][c]);
 		}
 		printf("\n");
 	}
 
-	for ( r = 0; r < R; r++) 
+	for ( r = 0; r < R ; r++) 
 	{
-		for ( c = 0; c < R; c++) 
+		for ( c = 0; c < r; c++) 
 		{
 			swap = data[r][c];
 			data[r][c] = data[c][r];
@@ -34,7 +34,6 @@ int main(void)
 		}
 		printf("\n");
 	}
-
 	printf("\n2d massive after swap:\n");
 
 	for ( r = 0; r < R; r++) 
